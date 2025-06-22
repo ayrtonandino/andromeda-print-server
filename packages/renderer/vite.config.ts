5f8c70e6300b88dbcf6a1f1bb12e6fbed01965fc
Ayrton Andino Bonazzola <60512432+ayrtonandino@users.noreply.github.com>
2025-06-22 14:07:30 -0300

    Update vite.config.ts

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     resolve: {
         alias: {
-            '@/': fileURLToPath(new URL('./src', import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 

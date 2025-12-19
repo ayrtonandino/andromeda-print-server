7270d0385deca4d02c74b491c5677a197adc3225
AyrtonAndino <60512432+ayrtonandino@users.noreply.github.com>
2025-12-19 12:56:15 -0300

    Update serverService.ts

--- packages/main/src/services/serverService.ts
+++ packages/main/src/services/serverService.ts
@@ -21,7 +21,10 @@ expressApp.use(bodyParser.urlencoded({ extended: false }))
 expressApp.use(bodyParser.json())
 
 expressApp.get('/connection', async (request, response) => {
-    response.json({ status: 'Conexión establecida correctamente.' })
+    response.json({
+        status: 'ok',
+        message: 'Conexión establecida correctamente.',
+    })
 })
 
 expressApp.get('/status', async (request, response) => {
@@ -163,13 +166,15 @@ export function startExpressServer(newPort?: number) {
 }
 
 export function restartExpressServer(newPort: number, oldPort?: number) {
-    if (server) {
+    if (server !== null) {
         server.close(() => {
             console.log(`Server stopped on previous port ${oldPort}!`)
 
             server = expressApp.listen(newPort, () => console.log(`Server restarted on port ${newPort}!`))
         })
-    } else {
-        startExpressServer()
+
+        return
     }
+
+    startExpressServer()
 }
